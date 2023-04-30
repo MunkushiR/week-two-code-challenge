8d9350fd62c11e71fcc3c6f1c21f53c5e327933e
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-05-01 00:16:10 +0300

    done

--- week-two-code-challenge/src/App.js
+++ week-two-code-challenge/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import BotsData from "./BotsData";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+     <div className="heading">
+      <h4>Bots Collection</h4>
+      <h4>My Bots Army</h4>
+     </div>
+     <BotsData />
+    </>
   );
 }
 
