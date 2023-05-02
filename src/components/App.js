975b831177a91f2e193951ec2e6ed2777ce72eca
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-05-02 18:49:56 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,64 +1,18 @@
-import React, { useEffect, useState } from "react";
-import BotsCollection from "./BotsCollection";
-import MyBotsArmy from "./MyBotsArmy";
+import React from "react";
+
+import BotsData from "./Fetch";
+
 function App() {
-  const [bots, setBots] = useState([]);
-  const [myBotsArmy, setMyBotsArmy] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:3000/bots")
-      .then((res) => res.json())
-      .then((data) => {
-        setBots(data);
-      });
-  }, []);
-  const botsElement = bots.map((bot) => {
-    return (
-      <BotsCollection
-        key={bot.id}
-        bot={bot}
-        AddMyBot={AddMyBot}
-        deleteBot={deleteBot}
-      />
-    );
-  });
-  function AddMyBot(item) {
-    if (!myBotsArmy.includes(item)) {
-      setMyBotsArmy((prevState) => {
-        return [...prevState, item];
-      });
-    }
-  }
-  const myBotElement = myBotsArmy.map((bot, index) => {
-    return (
-      <MyBotsArmy key={index} mybot={bot} removeMyBot={removeMyBot} />
-    );
-  });
-  function removeMyBot(element) {
-    setMyBotsArmy((prevState) => {
-      return prevState.filter((bot) => bot !== element);
-    });
-  }
-  function deleteBot(id) {
-    fetch(`http://localhost:3000/bots/${id}`, {
-      method: "DELETE",
-    })
-      .then(() => {
-        const updatedBots = bots.filter((bot) => bot.id !== id);
-        setBots(updatedBots);
-      })
-      .catch((err) => console.log(err));
-  }
   return (
-    <div className="container">
-      <div className="bots-collection">
-        <h2>Bots Collection</h2>
-        <div className="cards-container">{botsElement}</div>
-      </div>
-      <div className="my-bots-army">
-        <h2>My Bots Army</h2>
-        <div className="cards-container">{myBotElement}</div>
-      </div>
-    </div>
+    <>
+    
+     <div className="heading">
+      <h4>Bots Collection</h4>
+      <h4>My Bots Army</h4>
+     </div>
+     <BotsData />
+    </>
   );
 }
+
 export default App;
